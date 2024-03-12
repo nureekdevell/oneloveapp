883fb579fa278d6ab225e57ff5528587c6e07bf3
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-03-13 02:59:54 +0500

    layouts

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="container">
-    <header>
-      <nav>
+  <header class="bg w-full h-screen">
+    <div class="container">
+      <nav class="">
         <ul>
-          <li><img src="../../public/logo.svg" alt="" /></li>
-          <li></li>
+          <li>
+            <router-link to="/"
+              ><img src="../assets/img/logo.svg" alt="" />
+              <h1 class="text-base text-white">OneLove</h1></router-link
+            >
+          </li>
+          <li class="text-base text-white">
+            <router-link to="/">Впервые здесь?</router-link>
+            <Button />
+          </li>
         </ul>
       </nav>
-    </header>
-    <router-link :to="{ name: 'login' }">login</router-link>
-    <router-link :to="{ name: 'register' }">Register</router-link>
-    <router-view></router-view>
-  </div>
+
+      <router-view></router-view>
+    </div>
+  </header>
 </template>
 
-<script setup></script>
+<script setup>
+import Button from '@/ui-component/Button.vue'
+</script>
 
-<style scoped></style>
+<style scoped>
+.bg {
+  background-image: url('../public/bg.png');
+}
+</style>
